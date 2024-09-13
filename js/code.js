50e696894c77ab70540e60cfb0059f5bac1074fb
Cristy <132114818+CristyC123@users.noreply.github.com>
2024-09-13 19:33:38 -0400

    Added Contact Page

--- js/code.js
+++ js/code.js
@@ -69,14 +69,14 @@ function readCookie()
 		}
 	}
 	
-	//if( userId < 0 )
-	//{
-	//	window.location.href = "index.html";
-	//}
-	//else
-	//{
-	//	document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
-	//}
+	// if( userId < 0 )
+	// {
+	// 	window.location.href = "index.html";
+	// }
+	// else
+	// {
+	// 	document.getElementById("userName").innerHTML = "Logged in as " + firstName + " " + lastName;
+	// }
 }
 
 /**
