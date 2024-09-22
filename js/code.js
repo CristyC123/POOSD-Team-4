87648821b0d6718d4e2ae6d09fec0ea15c9d2952
SirPlepingham <146028531+SirPlepingham@users.noreply.github.com>
2024-09-22 00:33:35 -0400

    various front end changes
    redid colors with palette Christina made
    redid some css code with classes
    added submit button (for enter) to signupForm like Kier did to loginForm
    repositioned loginResult & signupResult to display properly
    added text to loginResult & signupResult
    repositioned signupForm w/ goSignup to account for loginResult
    added background image and tinted it (index & contacts)
    added image to title boxes and tinted them (index & contacts)
    got rid of alert boxes and replaced with red background & black text
    made invalidInput & resetInput for ease of use
    resized loginBox to account for loginResult message
    added onclicks to input fields to recolor when clicked

--- js/code.js
+++ js/code.js
@@ -15,15 +15,18 @@ let regex = /(?=.*[a-zA-Z])[a-zA-Z0-9-_]{3,18}$/;
  * @param { function } readyFunc Function to be executed with XMLHTTPRequest.onreadystatechange
  * @return { boolean } Success of request
  */
-function xhr(url, jsonPayload, id, readyFunc) {
+function xhr(url, jsonPayload, id, readyFunc)
+{
 	let xhreq = new XMLHttpRequest();
 	xhreq.open("POST", url, true);
 	xhreq.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try {
+	try
+	{
 		xhreq.onreadystatechange = readyFunc;
 		xhreq.send(jsonPayload);
 		return true;
-	} catch (err) {
+	} catch (err)
+	{
 		document.getElementById(id).innerHTML = err.message;
 		return false;
 	}
@@ -32,34 +35,40 @@ function xhr(url, jsonPayload, id, readyFunc) {
 /**
  * Saves cookie in browser for 20 minutes
  */
-function saveCookie() {
+function saveCookie()
+{
 	let minutes = 20;
 	let date = new Date();
-	date.setTime(date.getTime() + (minutes * 60 * 1000));
+	date.setTime(date.getTime()+(minutes*60*1000));	
 	document.cookie = "firstName=" + firstName + ",lastName=" + lastName + ",userId=" + userId + ";expires=" + date.toGMTString();
 }
 
 /**
  * Reads cookie from document and searches for relevant data
  */
-function readCookie() {
+function readCookie()
+{
 	userId = -1;
 	let data = document.cookie;
 	let splits = data.split(",");
-	for (var i = 0; i < splits.length; i++) {
+	for(var i = 0; i < splits.length; i++) 
+	{
 		let thisOne = splits[i].trim();
 		let tokens = thisOne.split("=");
-		if (tokens[0] == "firstName") {
+		if( tokens[0] == "firstName" )
+		{
 			firstName = tokens[1];
 		}
-		else if (tokens[0] == "lastName") {
+		else if( tokens[0] == "lastName" )
+		{
 			lastName = tokens[1];
 		}
-		else if (tokens[0] == "userId") {
-			userId = parseInt(tokens[1].trim());
+		else if( tokens[0] == "userId" )
+		{
+			userId = parseInt( tokens[1].trim() );
 		}
 	}
-
+	
 	// if( userId < 0 )
 	// {
 	// 	window.location.href = "index.html";
@@ -70,51 +79,67 @@ function readCookie() {
 	// }
 }
 
+//sets id's background color to red & placeholder text to black
+function invalidInput(id)
+{
+	id.style.backgroundColor = "#bc4e43";
+	id.classList.add("invalidClass");
+	id.value = "";
+}
+
+//resets id's background color & placeholder text to orignal values
+function resetInput(id)
+{
+	id.style.backgroundColor = "#f68456";
+	id.classList.remove("invalidClass");
+	id.value = "";
+}
+
 /**
  * Login request to server
  */
-function doLogin() {
+function doLogin()
+{
 	userId = 0;
 	firstName = "";
 	lastName = "";
-
+	
 	let login = document.getElementById("loginUser").value;
 	let password = document.getElementById("loginPass").value;
-	//	var hash = md5( password );
-
-	if (!validLogin(login, password)) {
-		document.getElementById("loginResult").innerHTML = "Invalid Login";
-		return;
-	}
-
+//	var hash = md5( password );
+	
+	if(!validLogin(login, password)) return;
+	
 	document.getElementById("loginResult").innerHTML = "";
-
+	
 	let tmp = {
 		login: login,
 		password: password
 	};
-	//	var tmp = {login:login,password:hash};
-	let jsonPayload = JSON.stringify(tmp);
-
+//	var tmp = {login:login,password:hash};
+	let jsonPayload = JSON.stringify( tmp );
+	
 	let url = urlBase + '/Login.' + extension;
 
 	xhr(url, jsonPayload, "loginResult", function () {
 		console.log(this.readyState + " " + this.status);
-		if (this.readyState == 4 && this.status == 200) {
+		if (this.readyState == 4 && this.status == 200) 
+		{
 			console.log(this.responseText);
 			let jsonObject = JSON.parse(this.responseText);
 			userId = jsonObject.id;
 
-			if (userId === undefined) {
+			if( userId === undefined ) {
 				document.getElementById("loginResult").innerHTML = "Connection could not be resolved. Please try again soon.";
 				return false;
 			}
-
-			if (userId < 1) {
+	
+			if( userId < 1 )
+			{		
 				document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
 				return false;
 			}
-
+	
 			firstName = jsonObject.firstName;
 			lastName = jsonObject.lastName;
 
@@ -122,43 +147,120 @@ function doLogin() {
 
 			window.location.href = "contacts.html";
 		}
-		else if (this.readyState == 4) {
+		else if (this.readyState == 4)
+		{
 			document.getElementById("loginResult").innerHTML = "Connection could not be resolved. Please try again later.";
 		}
 	});
 	return false;
 }
 
+//Switches the displayed form to login from sign up
+function goLogin()
+{
+	var log = document.getElementById("loginForm");
+	var sign = document.getElementById("signupForm");
+	var lbut = document.getElementById("goLoginButton");
+	var sbut = document.getElementById("goSignupButton");
+	
+	log.style.top = "0px";
+	sign.style.top = "400px";
+	lbut.style.top = "600px";
+	sbut.style.top = "500px";
+	
+	document.getElementById("loginResult").innerHTML = "";
+	resetInput(loginUser);
+	resetInput(loginPass);
+}
+
+//Checks if the login info is valid
+function validLogin(user, pass)
+{
+	let logRes = "Please input a ";
+	let userOk = true;
+	let passOk = true;
+	
+	if(user == "") 
+	{
+		console.log("Username is blank");
+		logRes += "username";
+		invalidInput(loginUser);
+		userOk = false;
+	}
+	else 
+	{
+		if(regex.test(user) == false)
+		{
+			console.log("Username is invalid");
+			logRes += "username";
+			invalidInput(loginUser);
+			userOk = false;
+		}
+		
+		console.log("Username is valid");
+	}
+	
+	if(pass == "") 
+	{
+		if(!userOk) logRes += " and <br/>";
+		console.log("Password is blank");
+		logRes += "password";
+		invalidInput(loginPass);
+		passOk = false;
+	}
+	else
+	{
+		if(regex.test(pass) == false)
+		{
+			if(!userOk) logRes += " and <br/>";
+			console.log("Password is invalid");
+			logRes += "valid password";
+			invalidInput(loginPass);
+			passOk = false;
+		}
+		
+		console.log("Password is valid");
+	}
+	
+	if(!userOk || !passOk) 
+	{
+		logRes += " to login";
+		document.getElementById("loginResult").innerHTML = logRes;
+		return false;
+	}
+	
+	return true;
+}
+
 /**
  * Sign up new user to server
  */
-function doSignup() {
+function doSignup()
+{
 	firstName = document.getElementById("signupFName").value;
 	lastName = document.getElementById("signupLName").value;
 	let login = document.getElementById("signupUser").value;
 	let password = document.getElementById("signupPass").value;
-	//	var hash = md5( password );
-
-	if (!validSignup(firstName, lastName, login, password)) {
-		document.getElementById("signupResult").innerHTML = "Invalid Signup";
-		return;
-	}
-
+//	var hash = md5( password );
+	
+	if(!validSignup(firstName, lastName, login, password)) return;
+	
 	document.getElementById("signupResult").innerHTML = "";
-
+	
 	let tmp = {
 		firstName: firstName,
 		lastName: lastName,
 		login: login,
 		password: password
 	};
-
-	let jsonPayload = JSON.stringify(tmp);
-
+	
+	let jsonPayload = JSON.stringify( tmp );
+	
 	let url = urlBase + '/Signup.' + extension;
-
-	xhr(url, jsonPayload, "signupResult", function () {
-		if (this.status == 409) {
+	
+	xhr(url, jsonPayload, "signupResult", function() {
+		if(this.status == 409)
+		{
 			document.getElementById("signupResult").innerHTML = "User already exists";
 			console.log("Sign up failed since user already exists");
 			alert("This user already exists please login");
@@ -167,13 +269,13 @@ function doSignup() {
 		
 		if (this.readyState == 4 && this.status == 200) 
 		{
-			let jsonObject = JSON.parse( this.responseText );
+			let jsonObject = JSON.parse(this.responseText);
 			userId = jsonObject.id;
 			firstName = jsonObject.firstName;
 			lastName = jsonObject.lastName;
-
+			
 			document.getElementById("signupResult").innerHTML = "User added";
-
+			
 			saveCookie();
 
 			window.location.href = "contacts.html";
@@ -181,10 +283,149 @@ function doSignup() {
 	});
 }
 
+//Switches the displayed form to sign up from login
+function goSignup()
+{
+	var log = document.getElementById("loginForm");
+	var sign = document.getElementById("signupForm");
+	var lbut = document.getElementById("goLoginButton");
+	var sbut = document.getElementById("goSignupButton");
+	
+	log.style.top = "-400px";
+	sign.style.top = "-218px";
+	lbut.style.top = "500px";
+	sbut.style.top = "600px";
+	
+	document.getElementById("signupResult").innerHTML = "";
+	resetInput(signupFName);
+	resetInput(signupLName);
+	resetInput(signupUser);
+	resetInput(signupPass);
+}
+
+//Checks if the sign up info is valid
+function validSignup(fname, lname, user, pass)
+{
+	let signResN = "Please input a ";
+	let signResC = "Please input a ";
+	let fnameOk = true;
+	let lnameOk = true;
+	let userOk = true;
+	let passOk = true;
+	
+	if(fname === "") 
+	{
+		console.log("First name is blank");
+		signResN += "first name";
+		invalidInput(signupFName);
+		fnameOk = false;
+	}
+	else 
+	{
+		if(regex.test(fname) === false)
+		{
+			console.log("First name is invalid");
+			signResN += "valid first name";
+			invalidInput(signupFName);
+			fnameOk = false;
+		}
+		
+		console.log("First name is valid");
+	}
+	
+	if(lname == "") 
+	{
+		if(!fnameOk) signResN += " and <br/>";
+		console.log("Last name is blank");
+		signResN += "last name";
+		invalidInput(signupLName);
+		lnameOk = false;
+	}
+	else
+	{
+		if(regex.test(lname) === false)
+		{
+			if(!fnameOk) signResN += " and <br/>";
+			console.log("Last name is invalid");
+			signResN += "valid last name";
+			invalidInput(signupLName);
+			lnameOk = false;
+		}
+		
+		console.log("Last name is valid");
+	}
+	
+	signResN += " to sign up";
+	
+	if(user === "") 
+	{
+		console.log("Username is blank");
+		signResC += "username";
+		invalidInput(signupUser);
+		userOk = false;
+	}
+	else 
+	{
+		if(regex.test(user) === false)
+		{
+			console.log("Username is invalid");
+			signResC += "valid username";
+			invalidInput(signupUser);
+			userOk = false;
+		}
+		
+		console.log("Username is valid");
+	}
+	
+	if(pass === "") 
+	{
+		if(!userOk) signResC += " and <br/>";
+		console.log("Password is blank");
+		signResC += "password";
+		invalidInput(signupPass);
+		passOk = false;
+	}
+	else
+	{
+		if(regex.test(pass) === false)
+		{
+			if(!userOk) signResC += " and <br/>";
+			console.log("Password is invalid");
+			signResC += "valid password";
+			invalidInput(signupPass);
+			passOk = false;
+		}
+		
+		console.log("Password is valid");
+	}
+	
+	signResC += " to sign up";
+	
+	if(!fnameOk || !lnameOk)
+	{
+		if(!userOk || !passOk)
+		{
+			document.getElementById("signupResult").innerHTML = signResN + "<br/>" + signResC;
+			return false;
+		}
+		document.getElementById("signupResult").innerHTML = signResN;
+		return false;
+	}
+	
+	if(!userOk || !passOk)
+	{
+		document.getElementById("signupResult").innerHTML = signResC;
+		return false;
+	}
+	
+	return true;
+}
+
 /**
  * Ends session and expires all cookies
  */
-function doLogout() {
+function doLogout()
+{
 	userId = 0;
 	firstName = "";
 	lastName = "";
@@ -195,7 +436,8 @@ function doLogout() {
 /**
  * Adds contact to user's contact list
  */
-function addContact() {
+function addContact()
+{
 	let newContact = document.getElementById("contactText").value;
 	document.getElementById("contactAddResult").innerHTML = "";
 
@@ -203,12 +445,13 @@ function addContact() {
 		contact: newContact,
 		userId: userId
 	};
-	let jsonPayload = JSON.stringify(tmp);
+	let jsonPayload = JSON.stringify( tmp );
 
 	let url = urlBase + '/Add.' + extension;
 
-	xhr(url, jsonPayload, "contactAddResult", function () {
-		if (this.readyState == 4 && this.status == 200) {
+	xhr(url, jsonPayload, "contactAddResult", function() {
+		if (this.readyState == 4 && this.status == 200) 
+		{
 			document.getElementById("contactAddResult").innerHTML = "Contact has been added";
 		}
 	});
@@ -217,33 +460,36 @@ function addContact() {
 /**
  * Fetches contact from user's contact list
  */
-function searchContacts() {
+function searchContacts()
+{
 	let srch = document.getElementById("searchText").value;
 	document.getElementById("contactSearchResult").innerHTML = "";
-
+	
 	let contactList = "";
 
 	let tmp = {
 		search: srch,
 		userId: userId
 	};
-	let jsonPayload = JSON.stringify(tmp);
-
+	let jsonPayload = JSON.stringify( tmp );
+	
 	let url = urlBase + '/Search.' + extension;
 
-	xhr(url, jsonPayload, "contactSearchResult", function () {
-		if (this.readyState == 4 && this.status == 200) {
+	xhr(url, jsonPayload, "contactSearchResult", function() {
+		if (this.readyState == 4 && this.status == 200) 
+		{
 			document.getElementById("contactSearchResult").innerHTML = "Contact(s) has been retrieved";
-			let jsonObject = JSON.parse( this.responseText );
+			let jsonObject = JSON.parse(this.responseText);
 			
 			for( let i=0; i<jsonObject.results.length; i++ )
 			{
 				contactList += jsonObject.results[i];
-				if (i < jsonObject.results.length - 1) {
+				if( i < jsonObject.results.length - 1 )
+				{
 					contactList += "<br />\r\n";
 				}
 			}
-
+			
 			document.getElementsByTagName("p")[0].innerHTML = contactList;
 		}
 	});
@@ -252,22 +498,24 @@ function searchContacts() {
 /**
  * Deletes contact from user's contact list
  */
-function deleteContact() {
-	if (!(confirm("Are you sure you want to delete this contact?"))) return;
-
+function deleteContact()
+{
+	if(!(confirm("Are you sure you want to delete this contact?"))) return;
+	
 	let delContact = document.getElementById("contactText").value;
 	document.getElementById("contactDeleteResult").innerHTML = "";
-
+	
 	let tmp = {
 		contact: delContact,
 		userId: userId
 	};
-	let jsonPayload = JSON.stringify(tmp);
-
+	let jsonPayload = JSON.stringify( tmp );
+	
 	let url = urlBase + '/Delete.' + extension;
 
-	xhr(url, jsonPayload, "contactDeleteResult", function () {
-		if (this.readyState == 4 && this.status == 200) {
+	xhr(url, jsonPayload, "contactDeleteResult", function() {
+		if (this.readyState == 4 && this.status == 200) 
+		{
 			document.getElementById("contactDeleteResult").innerHTML = "Contact has been deleted";
 		}
 	});
@@ -276,7 +524,8 @@ function deleteContact() {
 /**
  * Edits contact from user's contact list
  */
-function editContact() {
+function editContact()
+{
 	let newContact = document.getElementById("newContactText").value;
 	document.getElementById("contactEditResult").innerHTML = "";
 
@@ -284,175 +533,14 @@ function editContact() {
 		contact: newContact,
 		userId: userId
 	};
-	let jsonPayload = JSON.stringify(tmp);
+	let jsonPayload = JSON.stringify( tmp );
 
 	let url = urlBase + '/Edit.' + extension;
 
-	xhr(url, jsonPayload, "contactEditResult", function () {
-		if (this.readyState == 4 && this.status == 200) {
+	xhr(url, jsonPayload, "contactEditResult", function() {
+		if (this.readyState == 4 && this.status == 200)
+		{
 			document.getElementById("contactEditResult").innerHTML = "Contact has been edited";
 		}
 	});
 }
-
-
-//Switches the displayed form to login from sign up
-function goLogin() {
-	var log = document.getElementById("loginForm");
-	var sign = document.getElementById("signupForm");
-	var lbut = document.getElementById("goLoginButton");
-	var sbut = document.getElementById("goSignupButton");
-
-	log.style.top = "0px";
-	sign.style.top = "400px";
-	lbut.style.top = "500px";
-	sbut.style.top = "400px";
-}
-
-//Checks if the login info is valid
-function validLogin(user, pass) {
-	if (user == "") {
-		console.log("Username is blank");
-		alert("Please input a username to login");
-		return false;
-	}
-	else {
-		if (regex.test(user) == false) {
-			console.log("Username is invalid");
-			alert("Please input a valid username to login");
-			return false;
-		}
-
-		console.log("Username is valid");
-	}
-
-	if (pass == "") {
-		console.log("Password is blank");
-		alert("Please input a password to login");
-		return false;
-	}
-	else {
-		if (regex.test(pass) == false) {
-			console.log("Password is invalid");
-			alert("Please input a valid password to login");
-			return false;
-		}
-
-		console.log("Password is valid");
-	}
-
-	return true;
-}
-
-//Switches the displayed form to sign up from login
-function goSignup() {
-	var log = document.getElementById("loginForm");
-	var sign = document.getElementById("signupForm");
-	var lbut = document.getElementById("goLoginButton");
-	var sbut = document.getElementById("goSignupButton");
-
-	log.style.top = "-400px";
-	sign.style.top = "-125px";
-	lbut.style.top = "400px";
-	sbut.style.top = "500px";
-}
-
-//Checks if the sign up info is valid
-function validSignup(fname, lname, user, pass) {
-	if (fname === "") {
-		console.log("First name is blank");
-		alert("Please input a first name to sign up");
-		return false;
-	}
-	else {
-		if (regex.test(fname) === false) {
-			console.log("First name is invalid");
-			alert("Please input a valid first name to sign up");
-			return false;
-		}
-
-		console.log("First name is valid");
-	}
-
-	if (lname == "") {
-		console.log("Last name is blank");
-		alert("Please input a last name to sign up");
-		return false;
-	}
-	else {
-		if (regex.test(lname) === false) {
-			console.log("Last name is invalid");
-			alert("Please input a valid last name to sign up");
-			return false;
-		}
-
-		console.log("Last name is valid");
-	}
-
-	if (user === "") {
-		console.log("Username is blank");
-		alert("Please input a username to sign up");
-		return false;
-	}
-	else {
-		if (regex.test(user) === false) {
-			console.log("Username is invalid");
-			alert("Please input a valid username to sign up");
-			return false;
-		}
-
-		console.log("Username is valid");
-	}
-
-	if (pass === "") {
-		console.log("Password is blank");
-		alert("Please input a password to sign up");
-		return false;
-	}
-	else {
-		if (regex.test(pass) === false) {
-			console.log("Password is invalid");
-			alert("Please input a valid password to sign up");
-			return false;
-		}
-
-		console.log("Password is valid");
-	}
-
-	return true;
-}
-
-document.addEventListener('DOMContentLoaded', function () {
-	const contactList = document.getElementById('contactList');
-
-	contactList.addEventListener('click', function (event) {
-		const contactRow = event.target.closest('.contact-row');
-		if (contactRow && !event.target.closest('.edit-btn, .delete-btn')) {
-			toggleContact(contactRow);
-		}
-	});
-
-	function toggleContact(row) {
-		row.classList.toggle('expanded');
-		row.classList.toggle('collapsed');
-		const toggleIndicator = row.querySelector('.toggle-indicator');
-		toggleIndicator.textContent = row.classList.contains('expanded') ? '▲' : '▼';
-	}
-
-	let scrollToTopBtn = document.getElementById("scrollToTopBtn");
-
-	// Show the button when scrolling down 20px from the top
-	window.onscroll = function () {
-		if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-			scrollToTopBtn.style.display = "block";
-		} else {
-			scrollToTopBtn.style.display = "none";
-		}
-	};
-
-	// Scroll to the top of the document
-	scrollToTopBtn.onclick = function () {
-		window.scrollTo({ top: 0, behavior: 'smooth' });
-	};
-});
-
